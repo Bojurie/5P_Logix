319966104f230e20e68cac12d52705083e143bf0
Bojurie Rogers-Wright <40696649+Bojurie@users.noreply.github.com>
2020-09-20 12:22:05 -0700

    updated the faq section

---
+++ app.js
@@ -0,0 +1,26 @@
+
+function myDisplay() {
+  var x = document.getElementById("head");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function display() {
+  var x = document.getElementById("para");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function dispay() {
+  var x = document.getElementById('par')
+  if (x.style.display === 'none') {
+    x.style.display = 'block'
+  } else {
+    x.style.display = 'none'
+  }
+}
